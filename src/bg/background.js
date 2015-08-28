04cb8b43b64c328abe4b2034888ab5198aadb118
patrioticcow <cristi.catea@gmail.com>
2015-08-28 12:39:16 -0700

    updates

--- src/bg/background.js
+++ src/bg/background.js
@@ -4,11 +4,22 @@ chrome.tabs.query({}, function (tabs) {
 	}
 });
 
+chrome.tabs.onUpdated.addListener(function (id) {
+	chrome.tabs.get(id, function (tabs) {
+		if(tabs !== undefined) parseTabs(tabs.url, tabs.id);
+	});
+});
+
 chrome.tabs.onCreated.addListener(function (tabs) {
 	parseTabs(tabs.url, tabs.id);
 });
 
 function parseTabs(url, id) {
+	if (url.indexOf('google') > -1) return false;
+	if (url.indexOf('yahoo') > -1) return false;
+	if (url.indexOf('bing') > -1) return false;
+	if (url.indexOf('duckduckgo') > -1) return false;
+
 	chrome.storage.sync.get(function (resp) {
 		var isMatch = false;
 		for (var key in resp) {
@@ -27,7 +38,6 @@ function parseTabs(url, id) {
 				}
 			}
 			if (resp[key].type === '3') {
-				console.log(isMatch);
 				var re = new RegExp(resp[key].website);
 				if (re.test(url)) {
 					if (isMatch === false) chrome.tabs.remove(id);
